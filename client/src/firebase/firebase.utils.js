9efd87d3044ccbef92d4e39af487d3ab6ce61eca
Saurabh <saurabhshukla19011993@gmail.com>
2020-06-01 11:40:52 +0530

    updating sagas with code to create cart based on user

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -40,9 +40,22 @@ export const createUserProfileDocument = async (userAuth, addonData) => {
     return userRef;
 };
 
+export const getUserCartRef = async userId => {
+    const cartsRef = firestore.collection('carts').where('userId', '==', userId);
+    const snapShot = await cartsRef.get();
+
+    if(snapShot.empty) {
+        const cartDocRef = firestore.collection('carts').doc();
+        await cartDocRef.set({ userId, cartItems: [] });
+        return cartDocRef;
+    } else {
+        return snapShot.docs[0].ref;
+    }
+};
+
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = firestore.collection(collectionKey);
-    console.log(collectionRef);
+    // console.log(collectionRef);
     
     const batch = firestore.batch();
     objectsToAdd.forEach(obj => {
--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -33,11 +33,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                     cartItem => cartItem.id !== action.payload.id
                 )
             };
+
         case CartActionTypes.CLEAR_CART:
             return {
                 ...state,
                 cartItems: []
             }
+
+        case CartActionTypes.SET_CART_FROM_FIREBASE:
+            return {
+                ...state,
+                cartItems: action.payload
+            }
+
         default:
             return state;
     }
